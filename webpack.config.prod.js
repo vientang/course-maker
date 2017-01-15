f51cfb03d9e4d76af5ab3e176135a188097f6a02
Vien Tang <vientang@gmail.com>
2017-01-15 00:19:32 -0800

    add url-loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -40,8 +40,8 @@ module.exports = {
 			{test: /\.css$/, loader: ExtractTextPlugin.extract("css?sourceMap")},
 			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
       {test: /\.png$/, loader: "url-loader?mimetype=image/png" }
 		]
 	}
